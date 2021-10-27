903a6844cc1ab7013ff228305b5676cfa8816331
z13333 <92214624+z13333@users.noreply.github.com>
2021-10-27 21:39:32 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -4,10 +4,10 @@
 BASE_URL = "http://121.196.13.152:8080"
 
 LOGIN_INFO = {
-    # "username":"admin123",
-    # "password":"admin123"
-    "username":"testa01",
-    "password":"testa01"
+      "username":"admin123",
+      "password":"admin123
+    # "username":"testa01",
+    # "password":"testa01"
 }
 
 
